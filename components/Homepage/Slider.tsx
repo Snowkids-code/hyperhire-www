f3fe64bae7ffaaee031b553b9b278f3d4883cbcb
Snowkids-code <oumaj47@gmail.com>
2024-09-26 14:57:43 +0300

    revert slider styling changes

--- components/Homepage/Slider.tsx
+++ components/Homepage/Slider.tsx
@@ -45,7 +45,7 @@ export default function Slider() {
   
 
   return (
-    <div className="hidden laptop:grid secondary-container bg-[#81C3CF] ">
+    <div className="primary-container bg-[#81C3CF] ">
       <div style={{ gridArea: "main" }}>
         <div className="hidden laptop:grid p-4 py-14">
           <Carousel
