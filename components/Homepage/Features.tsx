b0491defb8d3fa1513353a233988d22d600f8616
Snowkids-code <oumaj47@gmail.com>
2024-09-26 09:21:20 +0300

    add features component

---
+++ components/Homepage/Features.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Features() {
+  return (
+    <div>
+        <hr />
+        <h1 className='mt-3 text-[#fff] font-[900] text-[18px] leading-[27px]'>평균 월 120만원</h1>
+        <p className='mt-3 text-white text-opacity-80 font-[900] text-[16px] leading-[27px]'>임금을 해당 국가를 기준으로 계산합니다.</p>
+    </div>
+  )
+}
